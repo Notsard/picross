745ba7f3352cfd5ddc986eb27464d2545f3ddf4c
maxime.simplon <maxime.chofflet.simplon@gmail.com>
2019-02-27 20:05:15 +0100

    boucle for

--- js/picross.js
+++ js/picross.js
@@ -1,96 +1,81 @@
-
 const LINE_NUMBER = 10;
 const SQUARE_PER_LINE = 10;
+const SQUARE_WIDTH = 50;
+const SQUARE_HEIGHT = 50;
+const SQUARE_MARGIN = 2;
+const SQUARE_COLORS = ["blue"];
 
-function draw() {
+
+
+function getCanva()
+{
+    return document.getElementById('canvasElem');
+}
+
+//
+function getContext() {
+    //
+    var elem = getCanva();
+
+   
+    //
+    return elem.getContext('2d');
+}
+
+//
+
+
+
+
+function squaresInit() {
+  //
+  squareArray = new Array(LINE_NUMBER);
+
+  for(var i = 0; i < LINE_NUMBER; i++) {
+      //
+      squareArray[i] = new Array(SQUARE_PER_LINE);
+
+      //
+      for(var j = 0; j < SQUARE_PER_LINE; j++) {
+          //
+          squareArray[i][j] = 1;
+      }
+  }
   
+}
 
-var c = document.getElementById('canvas1');
-var ctx = c.getContext("2d");
-
-// (1) Carré 250x250
-ctx.beginPath();      
-ctx.moveTo(50,50);   
-ctx.lineTo(250,50);  
-ctx.lineTo(250, 250);      
-ctx.lineTo(50, 250);      
-ctx.lineTo(50, 50); 
-ctx.fillStyle = "darkblue";
-ctx.fill();
-ctx.stroke();
-ctx.closePath();
-
-ctx.beginPath();      // (2) Carré 250x250
-ctx.moveTo(250, 50);   
-ctx.lineTo(450, 50);  
-ctx.lineTo(450, 250);  
-ctx.lineTo(250, 250); 
-ctx.lineTo(250, 50);
-ctx.fillStyle = "darkblue";
-ctx.fill();
-ctx.stroke();
-ctx.closePath();
-
-ctx.beginPath();      // (3) Carré 250x250
-ctx.moveTo(250, 250);   
-ctx.lineTo(250,450);  
-ctx.lineTo(450, 450);
-ctx.lineTo(450, 250);
-ctx.lineTo(50, 250);
-ctx.fillStyle = "darkblue";
-ctx.fill();   
-ctx.stroke();    
-ctx.closePath();
-
-ctx.beginPath();      // (4) Carré 250x250
-ctx.moveTo(50, 250);   
-ctx.lineTo(50,450);  
-ctx.lineTo(250,450); 
-ctx.lineTo(250, 250);
-ctx.lineTo(50, 250);
-ctx.fillStyle = "darkblue";
-ctx.fill();
-ctx.stroke();
-ctx.closePath(); }
-
-function square() {
-    var c = document.getElementById('canvas1');
-var ctx = c.getContext("2d");
-
-ctx.beginPath();      // Petit carré
-ctx.moveTo(50, 50);   
-ctx.lineTo(100,50);  
-ctx.lineTo(100,100);  
-ctx.lineTo(50, 100);  
-ctx.lineTo(50,50);  
-ctx.stroke();        
-ctx.closePath();
+function squaresDisplay(ctx) {
+  
+  for(var i = 0; i < squareArray.length; i++) {
+      ctx.fillStyle = SQUARE_COLORS[i];
+      
+      //
+      for(var j = 0; j < squareArray[i].length; j++) {
+          if(squareArray[i][j] == 1) {
+              ctx.fillRect(
+                  j * (SQUARE_WIDTH + SQUARE_MARGIN),
+                  i * (SQUARE_HEIGHT + SQUARE_MARGIN),
+                  SQUARE_WIDTH,SQUARE_HEIGHT);
+          }
+      }
   }
-
-  function rectangle() {
-    var c = document.getElementById('canvas1');
-var ctx = c.getContext("2d");
-    
-    ctx.beginPath();      
-    ctx.moveTo(460, 0);   
-    ctx.lineTo(460, 550);
-    ctx.lineTo(700, 550);
-    ctx.lineTo(700, 0);
-    ctx.fillStyle = "blue";
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
-
-  }
-
-window.addEventListener('load', function() {
-    draw(),
-    square(),
-    rectangle()
-});
+  
+}
 
 
 
+window.addEventListener('load', function () {
+  //
+  context = getContext();
+ 
 
-        
+  //
+  squaresInit(context);
 
+  //
+  squaresDisplay(context);
+
+  //
+ 
+
+}, );
