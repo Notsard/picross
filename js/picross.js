4d467689f360801241ab464543ba9f19b42aa640
Romain <romain.cecchini.simplon@gmail.com>
2019-03-12 15:35:54 +0100

    Ajout var tableau

--- js/picross.js
+++ js/picross.js
@@ -5,18 +5,38 @@ const SQUARE_HEIGHT = 50;
 const SQUARE_MARGIN = 1;
 const SQUARE_COLORS = ["blue"];
 const SQUARE_CLICK = ["black"];
-const ROWS = [[1],[2],[2],[3],[2]];
-const COLUMNS = [[0],[1],[5],[4],[0]];
+const ROWS = [
+    [1],
+    [2],
+    [2],
+    [3],
+    [2]
+];
+const COLUMNS = [
+    [0],
+    [1],
+    [5],
+    [4],
+    [0]
+];
 const TEXT_COL = [50, 100, 150, 200, 250];
 const TEXT_ROW = [100, 150, 200, 250, 300];
-var tabl = [];
-tabl[1]=[50,50];
-tabl[2]=[100,50];
-tabl[3]=[150,50];
-tabl[4]=[200,50];
-tabl[5]=[250,50];
+var tabl = [
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 1, 1, 0]
+];
+var tableauCanvas = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
 
-function getCanva(){
+function getCanva() {
     return document.getElementById('canvasElem');
 }
 
@@ -24,7 +44,7 @@ function getContext() {
     //
     var elem = getCanva();
 
-   
+
     //
     return elem.getContext('2d');
 }
@@ -35,38 +55,39 @@ function getContext() {
 
 
 function squaresInit() {
-  //
-  squareArray = new Array(LINE_NUMBER);
+    //
+    squareArray = new Array(LINE_NUMBER);
 
-  for(var i = 0; i < LINE_NUMBER; i++) {
-      //
-      squareArray[i] = new Array(SQUARE_PER_LINE);
+    for (var i = 0; i < LINE_NUMBER; i++) {
+        //
+        squareArray[i] = new Array(SQUARE_PER_LINE);
 
-      //
-      for(var j = 0; j < SQUARE_PER_LINE; j++) {
-          //
-          squareArray[i][j] = 1;
-      }
-  }
+        //
+        for (var j = 0; j < SQUARE_PER_LINE; j++) {
+            //
+            squareArray[i][j] = 1;
+        }
+    }
 }
 
 function squaresDisplay(ctx) {
-  
-  for(var i = 0; i < squareArray.length; i++) {
-      ctx.fillStyle = SQUARE_COLORS[i];
-      
-      //
-      for(var j = 0; j < squareArray[i].length; j++) {
-          if(squareArray[i][j] == 1) {
-              ctx.fillRect(
-                  j * (SQUARE_WIDTH + SQUARE_MARGIN),
-                  i * (SQUARE_HEIGHT + SQUARE_MARGIN),
-                  SQUARE_WIDTH,SQUARE_HEIGHT);
-          }
-      }
-  }
+
+    for (var i = 0; i < squareArray.length; i++) {
+        ctx.fillStyle = SQUARE_COLORS[i];
+
+        //
+        for (var j = 0; j < squareArray[i].length; j++) {
+            if (squareArray[i][j] == 1) {
+                ctx.fillRect(
+                    j * (SQUARE_WIDTH + SQUARE_MARGIN),
+                    i * (SQUARE_HEIGHT + SQUARE_MARGIN),
+                    SQUARE_WIDTH, SQUARE_HEIGHT);
+            }
+        }
+    }
 }
-function leftClick(event){
+
+function leftClick(event) {
     // on récupère le canva
     elem = getCanva();
 
@@ -76,94 +97,96 @@ function leftClick(event){
 
     // Les coordonnées de l'event - les coordonnées du canvas
     // On divise par la taille du carré
-    x = parseInt((event.clientX - rect.left) / (SQUARE_WIDTH + SQUARE_MARGIN ) ) * (SQUARE_WIDTH + SQUARE_MARGIN);
-    y = parseInt((event.clientY - rect.top)/ (SQUARE_HEIGHT + SQUARE_MARGIN) ) * (SQUARE_HEIGHT + SQUARE_MARGIN);
+    x = parseInt((event.clientX - rect.left) / (SQUARE_WIDTH + SQUARE_MARGIN)) * (SQUARE_WIDTH + SQUARE_MARGIN);
+    y = parseInt((event.clientY - rect.top) / (SQUARE_HEIGHT + SQUARE_MARGIN)) * (SQUARE_HEIGHT + SQUARE_MARGIN);
     var canvas = document.getElementById("canvasElem");
     var ctx = canvas.getContext("2d");
-    if (x==0 || y==0) {
+    if (x == 0 || y == 0) {
         return;
-    }
-    else{
+    } else {
         ctx.fillStyle = "black";
-         ctx.fillRect(x/SQUARE_MARGIN, y/SQUARE_MARGIN, 50, 50);
-    
-    }
-    }
+        ctx.fillRect(x / SQUARE_MARGIN, y / SQUARE_MARGIN, 50, 50);
 
-    function rightClick(event) {
-        elem = getCanva();
-
-        // On récupère les coordonnées du canva
-        var rect = elem.getBoundingClientRect();
-    
-    
-        // Les coordonnées de l'event - les coordonnées du canvas
-        // On divise par la taille du carré
-        x = parseInt((event.clientX - rect.left) / (SQUARE_WIDTH + SQUARE_MARGIN ) ) * (SQUARE_WIDTH + SQUARE_MARGIN);
-        y = parseInt((event.clientY - rect.top)/ (SQUARE_HEIGHT + SQUARE_MARGIN) ) * (SQUARE_HEIGHT + SQUARE_MARGIN);
-        var canvas = document.getElementById("canvasElem");
-        var ctx = canvas.getContext("2d");
-        if (x==0 || y==0) {
-            return;
-        }
-        else{
-            ctx.fillStyle = "blue";
-             ctx.fillRect(x, y, 50, 50);
-        
-        }
-        }
-        function juste(){
-            // var arg1 = alert("Test");
-            // var arg2 = alert("Test2");
-            for (var i = 0; i < tabl.length; i++) {
-                if (tabl== [3]){
-                    alert("Victoire");
-                }
-        }
     }
+    tableauCanvas[0][3] = 1;
+    tableauCanvas[1][3][4] = 1;
+    tableauCanvas[2][2][3][4] = 1;
+    tableauCanvas[3][3][4] = 1;
+}
+
+function rightClick(event) {
+    elem = getCanva();
+
+    // On récupère les coordonnées du canva
+    var rect = elem.getBoundingClientRect();
+
+
+    // Les coordonnées de l'event - les coordonnées du canvas
+    // On divise par la taille du carré
+    x = parseInt((event.clientX - rect.left) / (SQUARE_WIDTH + SQUARE_MARGIN)) * (SQUARE_WIDTH + SQUARE_MARGIN);
+    y = parseInt((event.clientY - rect.top) / (SQUARE_HEIGHT + SQUARE_MARGIN)) * (SQUARE_HEIGHT + SQUARE_MARGIN);
+    var canvas = document.getElementById("canvasElem");
+    var ctx = canvas.getContext("2d");
+    if (x == 0 || y == 0) {
+        return;
+    } else {
+        ctx.fillStyle = "blue";
+        ctx.fillRect(x, y, 50, 50);
+
+    }
+}
+
+function juste() {
+    var clic = tableauCanvas;
+    if (clic = 1) {
+        alert("Victoire");
+    } else {
+        alert("Perdu");
+    }
+}
 
 
 function rowText(ctx) {
-   for (var i=0; i<5; i++) {
-   
-    ctx.font = "25px Arial";
-    ctx.fillStyle = 'white';
-    ctx.fillText(ROWS[i], 0, TEXT_ROW[i] ); 
-}
+    for (var i = 0; i < 5; i++) {
+
+        ctx.font = "25px Arial";
+        ctx.fillStyle = 'white';
+        ctx.fillText(ROWS[i], 0, TEXT_ROW[i]);
+    }
 
 }
 
 function colText(ctx) {
-    for (var i=0; i<7; i++) {
-    
-     ctx.font = "25px Arial";
-     ctx.fillStyle = 'white';
-     ctx.fillText(COLUMNS[i], TEXT_COL[i] +(SQUARE_MARGIN + i), SQUARE_HEIGHT); 
- }
- 
- }
+    for (var i = 0; i < 7; i++) {
+
+        ctx.font = "25px Arial";
+        ctx.fillStyle = 'white';
+        ctx.fillText(COLUMNS[i], TEXT_COL[i] + (SQUARE_MARGIN + i), SQUARE_HEIGHT);
+    }
+
+}
 
 
- document.oncontextmenu = new Function("return false");
+document.oncontextmenu = new Function("return false");
 
 window.addEventListener('load', function () {
-  //
-  context = getContext();
+    //
+    context = getContext();
 
-  elem = getCanva();
-  elem.addEventListener('click', leftClick);
-  elem.addEventListener("contextmenu", rightClick);
+    elem = getCanva();
+    elem.addEventListener('click', leftClick);
+    elem.addEventListener("contextmenu", rightClick);
 
 
-  //
-  squaresInit(context);
+    //
+    squaresInit(context);
 
-  //
-  squaresDisplay(context);
+    //
+    squaresDisplay(context);
 
-  //
-rowText(context);
+    //
+    rowText(context);
 
-colText(context);
+    colText(context);
 
 }, );
